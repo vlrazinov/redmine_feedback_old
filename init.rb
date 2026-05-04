3cab0ed653b43c986528dc8578c9950282f49e1f
Владислав Разинов <razinov63@gmail.com>
2026-05-04 09:28:09 +0000

    Fix Redmine plugin initialization hook

--- init.rb
+++ init.rb
@@ -40,87 +40,10 @@ Redmine::Plugin.register :redmine_feedback do
     'feedback_comment_custom_field_id' => nil,
     'feedback_link_text' => 'Оценить поддержку'
   }, :partial => 'settings/feedback_settings'
-  
-  # Хук после загрузки плагина - создаём и привязываем поля автоматически
-  Redmine::Plugin.after_initialize do
-    ensure_feedback_field!
-    ensure_feedback_comment_field!
-  end
-  
-  def self.ensure_feedback_field!
-    field_id = Setting.plugin_redmine_feedback['feedback_custom_field_id']
-    
-    # Если поле уже настроено, проверяем существует ли оно
-    if field_id.present?
-      existing_field = IssueCustomField.find_by(id: field_id)
-      return if existing_field && existing_field.name == 'Оценка поддержки'
-    end
-    
-    # Ищем поле по имени
-    existing_field = IssueCustomField.find_by(name: 'Оценка поддержки')
-    if existing_field
-      Setting.plugin_redmine_feedback = Setting.plugin_redmine_feedback.merge(
-        'feedback_custom_field_id' => existing_field.id.to_s
-      )
-      return
-    end
-    
-    # Создаём новое поле
-    field = IssueCustomField.create!(
-      name: 'Оценка поддержки',
-      field_format: 'string',
-      is_for_all: true,
-      is_filter: true,
-      editable: true,
-      visible: true,
-      trackers: Tracker.all
-    )
-    
-    Setting.plugin_redmine_feedback = Setting.plugin_redmine_feedback.merge(
-      'feedback_custom_field_id' => field.id.to_s
-    )
-  rescue => e
-    Rails.logger.error "[Redmine Feedback] Error creating feedback custom field: #{e.message}"
-  end
-
-  def self.ensure_feedback_comment_field!
-    field_id = Setting.plugin_redmine_feedback['feedback_comment_custom_field_id']
-    
-    # Если поле уже настроено, проверяем существует ли оно
-    if field_id.present?
-      existing_field = IssueCustomField.find_by(id: field_id)
-      return if existing_field && existing_field.name == 'Комментарий к оценке поддержки'
-    end
-    
-    # Ищем поле по имени
-    existing_field = IssueCustomField.find_by(name: 'Комментарий к оценке поддержки')
-    if existing_field
-      Setting.plugin_redmine_feedback = Setting.plugin_redmine_feedback.merge(
-        'feedback_comment_custom_field_id' => existing_field.id.to_s
-      )
-      return
-    end
-    
-    # Создаём новое поле
-    field = IssueCustomField.create!(
-      name: 'Комментарий к оценке поддержки',
-      field_format: 'text',
-      is_for_all: true,
-      is_filter: true,
-      editable: true,
-      visible: true,
-      trackers: Tracker.all
-    )
-    
-    Setting.plugin_redmine_feedback = Setting.plugin_redmine_feedback.merge(
-      'feedback_comment_custom_field_id' => field.id.to_s
-    )
-  rescue => e
-    Rails.logger.error "[Redmine Feedback] Error creating feedback comment custom field: #{e.message}"
-  end
 end
 
-# Загружаем хук после регистрации плагина
 Rails.configuration.to_prepare do
+  require_dependency 'redmine_feedback/custom_fields_manager'
   require_dependency 'redmine_feedback/hooks'
+  RedmineFeedback::CustomFieldsManager.ensure_custom_fields_exist!
 end
